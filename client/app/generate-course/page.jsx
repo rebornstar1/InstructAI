ee2288d17c7c98cfdf8350fa1ff96bb97d3fbd2f
Sanjay Paul <rebornstar677@gmail.com>
2025-04-15 11:26:24 +0530

    suspense import added

--- client/app/generate-course/page.jsx
+++ client/app/generate-course/page.jsx
@@ -7,6 +7,7 @@ import { Star } from "lucide-react";
 import { useAuth } from "@/context/AuthContext";
 import { fetchWithAuth } from "@/lib/api";
 import { usePathname, useSearchParams } from 'next/navigation';
+import { Suspense } from "react";
 
 import CourseCreationComponent from "../../components/CourseCreationComponent";
 
